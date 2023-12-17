162f8cdeb74eae6f7151c3ced954db562efe7b15
massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-12-17 11:47:18 +0100

    added frontend files

---
+++ app/frontend/src/pages/About.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+    /**
+     * route param: /about/:name
+     */
+    name: string;
+}>();
+</script>
+
+<template>
+    <div data-testid="about-content">
+        This is page about {{ name }}
+    </div>
+</template>
