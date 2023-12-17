80ec9554cc58e0108f0ed35fe613a73aadcaa64f
massfice <adrian.marian.tomasz.larysz@gmail.com>
2023-12-17 11:46:44 +0100

    added backend files

---
+++ app/backend/index.ts
@@ -0,0 +1,11 @@
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { createContext } from './src/trpc';
+import { router } from './src/trpc/router';
+
+export type Router = typeof router;
+
+export const trpcMiddleware =
+    trpcExpress.createExpressMiddleware({
+        router,
+        createContext,
+    });
